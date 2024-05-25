541a755d56b1f811e9e876ec7041deb8fcc667ca
kokomi0 <candytoy000@gmail.com>
2024-05-25 20:36:07 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,24 @@ import Navigation from "@/components/home/Navigation";
 import Main from "@/components/home/Main";
 
 export default function Home() {
-  let counter=0
+  let counter=1
+  const list = [
+    {id:1,value:"content1"},
+    {id:2,value:"content2"},
+    {id:3,value:"content3"},
+    {id:4,value:"content4"}
+  ]
   return (
     <div className="bg-yellow-500 p-10">
-     <Navigation/>
-     <Main counter={counter}/>
-     counter:{counter}
+      <Navigation />
+      <Main counter={counter}/>
+     counter: {counter}
+     {counter=== 0 ? <p>计数器没有启动</p> : <p>计数器：{counter}</p> }
+     <ul>
+      {list.map((item) => {
+        return <li key={item.id}>{item.value}</li>
+      })}
+     </ul>
     </div>
-  );
+  )
 }
--- components/home/Main.tsx
+++ components/home/Main.tsx
@@ -1,3 +1,3 @@
-export default function Main({counter}:{counter:number}) {
+export default function Main({counter}:{ counter:number}) {
     return <main>主体内容： {counter}</main>
 }
